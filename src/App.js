b2a9bd3a366a63264ef951deb79641be46b57fa5
Kirby <jkirbycampbell@gmail.com>
2018-11-14 09:50:15 -0800

    Test commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload Also this is a test testttff.
+            Edit <code>src/App.js</code> and save to reload Also this is a test testttffgg.
           </p>
           <a
             className="App-link"
