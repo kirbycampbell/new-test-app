545758c301dcf5b962c55216caeedb6a89a4e333
kirbycampbell <jkirbycampbell@gmail.com>
2018-11-14 09:56:03 -0800

    testing commits and branches

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload Also this is a test testttffgg.
+            Edit <code>src/App.js</code> and save to reload Also this is a test testttffggggg.
           </p>
           <a
             className="App-link"
