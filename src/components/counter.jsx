f02b0696d3bcddf77afdfcc6348fa48a5529deb9
kirbycampbell <jkirbycampbell@gmail.com>
2018-11-15 09:36:27 -0800

    End of this section

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,17 +11,17 @@ class Counter extends Component {
   // }
 
   //Using an arrow constructor instead of the above ^^^
-  handleIncrement = () => {
-    console.log("Increment clicked", this);
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
-    let classes = this.getBadgeClasses();
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
