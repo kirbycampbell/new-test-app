3f942e3ca788f641e3c717e7cdd4c4030711a7c3
kirbycampbell <jkirbycampbell@gmail.com>
2018-11-15 09:13:48 -0800

    Adding map function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,32 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
+    let classes = this.getBadgeClasses();
+
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={classes}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
+  //control + shift + r will auto refactor code. This is the output of that below
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
